f3151a6344d3906579164e6f3d528de52ed479e1
Ajay Rawat <a.rawat@saedubai.com>
2018-02-28 15:44:41 +0400

    Added Hamdi's First Person Controller and Camera Controller in the main game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    Vector2 mouselook;
+    Vector2 smoothV;
+    public float sensetivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        var mouseMovement = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseMovement = Vector2.Scale(mouseMovement, new Vector2(sensetivity * smoothing, sensetivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseMovement.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseMovement.y, 1f / smoothing);
+        mouselook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouselook.x, character.transform.up);
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float playerSpeed = 10.0f;
+
+	void Start ()
+    {
+        // to lock the mouse to the game
+        //Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+
+	void Update ()
+    {
+        float fAndB = Input.GetAxis("Vertical") * playerSpeed;
+        float rAndL = Input.GetAxis("Horizontal") * playerSpeed;
+
+        //to make character to move smothely
+        fAndB *= Time.deltaTime;
+        rAndL *= Time.deltaTime;
+
+        //
+        transform.Translate(rAndL, 0, fAndB);
+
+        //to cancel the mouse lock
+        /*if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }*/
+	}
+}
