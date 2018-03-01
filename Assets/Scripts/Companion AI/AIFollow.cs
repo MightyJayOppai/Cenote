fdbfc4d9ee45e2d63828e46359145622b4a2f12f
Ajay Rawat <a.rawat@saedubai.com>
2018-03-01 11:55:51 +0400

    Added a Companion gameobject as well as a script to be updated

---
+++ Assets/Scripts/Companion AI/AIFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour {
+
+    Transform Player;
+    public float CompanionSpeed = 3;
+   
+	void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	
+	void Update ()
+    {
+        transform.LookAt(Player);
+        AICompanion();
+	}
+
+    void AICompanion ()
+    {
+        transform.position += transform.forward * CompanionSpeed * Time.deltaTime;
+    }
+}
