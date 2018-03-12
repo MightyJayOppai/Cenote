2132ffee4a860e578b19c6c29033c3679fa57638
Ajay Rawat <a.rawat@saedubai.com>
2018-03-12 17:38:35 +0400

    Added a gatecontroller script to open the gate when a button is pushed

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour {
+
+    public GameObject Gate;
+    public bool gateIsOpening;
+    public GameObject GateButtonText;
+
+	void Start ()
+    {
+        GateButtonText.SetActive(false);
+	}
+	
+	
+	void Update ()
+    {
+        //if the boolian is true, open the gate
+        if (gateIsOpening == true)
+        {
+            Gate.transform.Translate(Vector3.up * Time.deltaTime * 2);
+        }
+
+        //if the position of the gate is less than 10f, stop the gate
+        if (Gate.transform.position.y > 10f)
+        {
+            gateIsOpening = false;
+        }
+
+        // Once the button is clicked, the gate will open
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            gateIsOpening = true;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //To display the GateText
+        if (other.tag == "Player")
+        {
+            GateButtonText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //To remove the GateText 
+        if (other.tag == "Player")
+        {
+            GateButtonText.SetActive(false);
+        }
+    }
+}
