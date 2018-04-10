b8000933e8ebadf40e175b72157959797dd409e7
Ajay Rawat <a.rawat@saedubai.com>
2018-04-10 17:40:51 +0400

    Just moved the note placement (very small change)

---
+++ Assets/Scripts/Ajay's Scripts/Companion AI/AIDetectImp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDetectImp : Node
+{
+    public override void Execute(CompanionBehaviorTree MainBT)
+    {
+        
+    }
+}
