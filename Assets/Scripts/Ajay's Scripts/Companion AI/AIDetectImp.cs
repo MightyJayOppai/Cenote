ac52147ed3755384c87423327d1569908c2f783b
Ajay Rawat <a.rawat@saedubai.com>
2018-04-16 18:12:25 +0400

    Added the textured models as well as the animations in unity

--- Assets/Scripts/Ajay's Scripts/Companion AI/AIDetectImp.cs
+++ Assets/Scripts/Ajay's Scripts/Companion AI/AIDetectImp.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIDetectImp : Node
+public class AIDetectImp : MonoBehaviour
 {
-    public override void Execute(CompanionBehaviorTree MainBT)
+    public float speed;
+    public LayerMask layer;
+
+    void Start()
     {
         
+    }
+
+    void Update()
+    {
+
+        Collider[] Hits = Physics.OverlapSphere(transform.position, 10 , layer);
+        if(Hits.Length > 0)
+        {
+            float Step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Hits[0].transform.position, Step);
+        }
+               
+        //foreach (Collider hit in Hits)
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, 10f);
     }
 }
--- Assets/Scripts/Ajay's Scripts/Companion AI/CompanionBehaviorTree.cs
+++ Assets/Scripts/Ajay's Scripts/Companion AI/CompanionBehaviorTree.cs
@@ -11,8 +11,12 @@ public class CompanionBehaviorTree : MonoBehaviour {
     public float CompanionFollowDistance;
     public float DistanceToPlayer;
 
+    public Animator anim;
+
 	void Start ()
     {
+        anim = GetComponent<Animator>();
+
         Selector selectNode = new Selector();
         Sequencer sequencerNode = new Sequencer();
 
